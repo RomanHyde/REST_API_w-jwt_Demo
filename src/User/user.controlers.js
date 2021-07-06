d1eac3e5bb74953785480dca9f77705ee93e9b70
RomanHyde <romanhyde1@gmail.com>
2021-07-06 14:51:28 +0100

    #2 added create user to  user controler

--- src/User/user.controlers.js
+++ src/User/user.controlers.js
@@ -1,9 +1,20 @@
-const { user } = require('./user.model');
+const { User } = require('./user.model');
 exports.getUser = async (req, res) => {
     try {
         res.status(200).send(req.user)
         
     } catch (error) {
         res.status(500).send({message: "User not found"});
+    };
+};
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const token = await user.generateAuthToken();
+        const savedUser = await user.save();
+        res.status(200).send(savedUser, token)
+    } catch (error) {
+        res.status(500).send({ message: "Could not create user" });
     };
 };
